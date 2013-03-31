eedf31428d8dfb69589bd669458eb63c174e40fa
Ceri Storey <cez@necrofish.org.uk>
2013-03-31 18:48:40 +0100

    Refactor the Decks spacing algorithm to use algorithms.

--- lib/srsrb/decks.rb
+++ lib/srsrb/decks.rb
@@ -6,18 +6,26 @@ module SRSRB
     def initialize event_store
       self.event_store = event_store
       self.next_due_dates = Hamster.hash
+      self.intervals = Hamster.hash
     end
 
     def score_card! card_id, score
       prev_due_date = next_due_dates.fetch(card_id, 0)
-      next_due_date = (prev_due_date.succ * 2) - 1
+      prev_interval = intervals.fetch(card_id, 0.5)
+
+      interval = prev_interval * 2
+
+      next_due_date = prev_due_date + interval
+
       pp card: card_id.to_s, prev_due_date: prev_due_date, next: next_due_date
+
       self.next_due_dates = next_due_dates.put(card_id, next_due_date)
+      self.intervals = intervals.put(card_id, interval)
 
       event_store.record! card_id, CardReviewed.new(score: score, next_due_date: next_due_date)
     end
 
     private
-    attr_accessor :event_store, :next_due_dates
+    attr_accessor :event_store, :next_due_dates, :intervals
   end
 end

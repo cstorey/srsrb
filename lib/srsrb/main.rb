2558ab9b77d68dee0dc68f37ccd71124ba324cc0
Ceri Storey <cez@necrofish.org.uk>
2013-04-06 21:44:20 +0100

    Convert Main into an actual class, and use lazy initialisation to construct objects.

--- lib/srsrb/main.rb
+++ lib/srsrb/main.rb
@@ -5,17 +5,40 @@ require 'srsrb/object_patch'
 require 'srsrb/rackapp'
 
 module SRSRB
-  module Main
-    def self.assemble
-      event_store = EventStore.new
-      deck_changes = Decks.new event_store
-      deck = DeckViewModel.new event_store
-      deck.start!
+  module Lazy
+    def lazy name, &generator
+      ivar = :"@__#{name}"
+      define_method name do
+        if not value = instance_variable_get(ivar)
+          value = instance_eval &generator
+          instance_variable_set ivar, value
+        end
+        value
+      end
+      pp instance_methods: instance_methods(false)
+    end
+  end
 
-      app = ReviewsApp.new deck, deck_changes
-      app = CardEditorApp.new deck, deck_changes, app
-      app = ModelEditorApp.new deck, deck_changes, app
-      SystemTestHackApi.new(app, deck, deck_changes)
+  class Main
+    def self.assemble
+      self.new.assemble
+    end
+
+    extend Lazy
+
+    lazy(:event_store) { EventStore.new }
+    lazy(:deck_changes) { Decks.new event_store }
+    lazy(:deck) { DeckViewModel.new event_store }
+
+    # We really want layered mixins for this.
+    lazy(:app0) { ReviewsApp.new deck, deck_changes }
+    lazy(:app1) { CardEditorApp.new deck, deck_changes, app0 }
+    lazy(:app2) { ModelEditorApp.new deck, deck_changes, app1 }
+    lazy(:app3) { SystemTestHackApi.new(app2, deck, deck_changes) }
+
+    def assemble
+      deck.start!
+      app3
     end
   end
 end

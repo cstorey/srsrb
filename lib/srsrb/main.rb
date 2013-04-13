84f0831409d82f2b469021c8d40a1c4d68459762
Ceri Storey <cez@necrofish.org.uk>
2013-04-13 17:38:51 +0100

    Move away from the lazy helper in Main in a vain attempt to appease CodeClimate.

--- lib/srsrb/main.rb
+++ lib/srsrb/main.rb
@@ -5,45 +5,51 @@ require 'srsrb/object_patch'
 require 'srsrb/rackapp'
 
 module SRSRB
-  module Lazy
-    def lazy name, &generator
-      ivar = :"@__#{name}"
-      define_method name do
-        if not value = instance_variable_get(ivar)
-          value = instance_eval &generator
-          instance_variable_set ivar, value
-        end
-        value
-      end
-    end
-  end
-
   class Main
     def self.assemble
       self.new.assemble
     end
 
-    extend Lazy
-
-    lazy(:event_store) { EventStore.new }
-    lazy(:models) { Models.new event_store }
-    lazy(:model_editing) { ModelEditing.new event_store }
-    lazy(:deck_reviews) { ReviewScoring.new event_store }
-    lazy(:card_editing) { CardEditing.new event_store, models }
-    lazy(:deck) { DeckViewModel.new event_store }
+    def event_store
+      @event_store ||= EventStore.new
+    end
+    def models
+      @models ||= Models.new event_store
+    end
+    def model_editing
+      @model_editing ||= ModelEditing.new event_store
+    end
+    def deck_reviews
+      @deck_reviews ||= ReviewScoring.new event_store
+    end
+    def card_editing
+      @card_editing ||= CardEditing.new event_store, models
+    end
+    def deck
+      @deck ||= DeckViewModel.new event_store
+    end
 
     # We really want layered mixins for this.
-    lazy(:app0) { ReviewsApp.new deck, deck_reviews }
-    lazy(:app1) { CardEditorApp.new deck, card_editing, app0 }
-    lazy(:app2) { ModelEditorApp.new deck, model_editing, app1 }
-    lazy(:app3) { SystemTestHackApi.new(app2, deck, card_editing, model_editing) }
+    def app0
+      ReviewsApp.new deck, deck_reviews
+    end
+    def app1
+      CardEditorApp.new deck, card_editing, app0
+    end
+    def app2
+      ModelEditorApp.new deck, model_editing, app1
+    end
+    def app3
+      SystemTestHackApi.new(app2, deck, card_editing, model_editing)
+    end
 
-    lazy(:app4) { Rack::Session::Pool.new app3, :key => 'rack.session',
+    def app4
+      Rack::Session::Pool.new app3, :key => 'rack.session',
                            #:domain => 'foo.com',
                            :path => '/',
                            :expire_after => 2592000, # In seconds
                            :secret => 'change_me'
-    }
+    end
 
     def assemble
       deck.start!

582d76c2f8fb70b892393b5564b59e3f5a0ee516
Ceri Storey <cez@necrofish.org.uk>
2013-05-10 15:01:56 +0100

    Basic implementation of importing anki decks. Does not handle schedules, or multiple cards per fact as of yet.

--- lib/srsrb/anki_import_parser.rb
+++ lib/srsrb/anki_import_parser.rb
@@ -1,11 +1,13 @@
 require 'sqlite3'
 require 'sequel'
 
-# TODO: Add another model and some basic cards to the example data.
+# TODO: Add another model and some basic cards to the example data. Eg: a
+# vocab model, with forward and reverse cards.
 module SRSRB
   class AnkiImportParser
     def initialize model_editing, card_editing
       self.model_editing = model_editing
+      self.card_editing = card_editing
     end
 
     def accept_upload file
@@ -15,14 +17,29 @@ module SRSRB
 
     private
     def import_models db
+      model_id = LexicalUUID.new
       db[:models].all.each do |r|
-        model_editing.name_model! LexicalUUID.new, r.fetch(:name)
+        model_editing.name_model! model_id, r.fetch(:name)
         db[:fieldModels].each do |fm|
-          model_editing.add_model_field! LexicalUUID.new, fm.fetch(:name)
+          model_editing.add_model_field! model_id, fm.fetch(:name)
         end
+      end
+
+      db[:cardModels].select_map([:qformat, :aformat]).each do |(question, answer)|
+          model_editing.edit_model_templates! model_id, question, answer
+      end
+
+      db[:facts].
+        join(:fields, :factId => :id).
+        join(:fieldModels, :id => :fieldModelId).
+        select_map([:facts__id, :fieldModels__name, :fields__value]).
+        group_by(&:first).
+        each do |fact_id, grouped|
+          data = grouped.inject(Hamster.hash) { |h, (_, field, value)| h.put(field, value) }
+          card_editing.add_or_edit_card! LexicalUUID.new, model_id, data
       end
     end
 
-    attr_accessor :model_editing
+    attr_accessor :model_editing, :card_editing
   end
 end
--- spec/05_integration/srsrb/anki_import_parser_spec.rb
+++ spec/05_integration/srsrb/anki_import_parser_spec.rb
@@ -1,3 +1,4 @@
+# coding: utf-8
 require 'srsrb/anki_import_parser'
 
 module SRSRB
@@ -13,6 +14,8 @@ module SRSRB
     before do
       model_editing.stub(:name_model!)
       model_editing.stub(:add_model_field!)
+      model_editing.stub(:edit_model_templates!)
+      card_editing.stub(:add_or_edit_card!)
     end
 
     it "should first parse and create a model" do
@@ -26,6 +29,51 @@ module SRSRB
       model_editing.should_receive(:add_model_field!).with(an_instance_of(LexicalUUID), 'Romanzied')
       model_editing.should_receive(:add_model_field!).with(an_instance_of(LexicalUUID), 'Sound')
       parser.accept_upload hangul
+    end
+
+    it "should create the fields for the same model" do
+      ids = Set.new
+      model_editing.should_receive(:name_model!) { |id, _| ids << id }
+      model_editing.should_receive(:add_model_field!) { |id, _| ids << id }
+      parser.accept_upload hangul
+
+      expect(ids).to have(1).item
+    end
+
+    it "should extract templates for the given model" do
+      model_editing.should_receive(:edit_model_templates!).
+        with(an_instance_of(LexicalUUID), an_instance_of(String), an_instance_of(String))
+      parser.accept_upload hangul
+    end
+
+    it "should create cards for each fact in the deck" do
+      card_editing.should_receive(:add_or_edit_card!).
+        with(an_instance_of(LexicalUUID), an_instance_of(LexicalUUID), anything).
+        exactly(37).times
+      parser.accept_upload hangul
+    end
+
+    it "should create them with the given model_id" do
+      model_id = nil
+      card_models = {}
+      model_editing.stub(:name_model!) { |id, _| model_id = id }
+      card_editing.stub(:add_or_edit_card!) { |id, model_id, _| card_models[id] = model_id }
+
+      parser.accept_upload hangul
+      expect(Set.new(card_models.values)).to be == Set.new([model_id])
+    end
+
+    it "should import the data from the deck" do
+      cards = {}
+      card_editing.stub(:add_or_edit_card!) { |id, _, fields| cards[id] = fields }
+
+      parser.accept_upload hangul
+
+      expect(cards.values).to be_any { |fields|
+        fields["Hangul"] == '애' &&
+          fields["Romanzied"] == 'ae' &&
+          fields["Sound"] == '[sound:ko_vx_26.mp3]'
+      }
     end
   end
 end

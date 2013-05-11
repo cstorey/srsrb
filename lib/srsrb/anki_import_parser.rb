6005190b43ca8ab1e62eb540beaef67055c5b7cc
Ceri Storey <cez@necrofish.org.uk>
2013-05-11 16:02:54 +0100

    Update to support importing multiple models from a deck, but not yet multiple card formats per model.

--- lib/srsrb/anki_import_parser.rb
+++ lib/srsrb/anki_import_parser.rb
@@ -17,27 +17,36 @@ module SRSRB
 
     private
     def import_models db
-      model_id = LexicalUUID.new
-      db[:models].all.each do |r|
-        model_editing.name_model! model_id, r.fetch(:name)
-        db[:fieldModels].each do |fm|
+      model_id = nil
+      db[:models].all.each do |model|
+        model_id = LexicalUUID.new
+        model_editing.name_model! model_id, model.fetch(:name)
+        db[:fieldModels].where(modelId: model[:id]).order_by(:ordinal).each do |fm|
           model_editing.add_model_field! model_id, fm.fetch(:name)
+        end
+
+        db[:cardModels].
+          where(modelId: model[:id]).
+          limit(1).
+          select_map([:qformat, :aformat]).
+          each do |(question, answer)|
+            model_editing.edit_model_templates! model_id, question, answer
+        end
+
+        db[:facts].
+          where(facts__modelId: model[:id]).
+          join(:fields, :factId => :id).
+          join(:fieldModels, :id => :fieldModelId).
+          select_map([:facts__id, :fieldModels__name, :fields__value]).
+          group_by(&:first).
+          each do |fact_id, grouped|
+            data = grouped.inject(Hamster.hash) { |h, (_, field, value)| h.put(field, value) }
+            card_editing.add_or_edit_card! LexicalUUID.new, model_id, data
         end
       end
 
-      db[:cardModels].select_map([:qformat, :aformat]).each do |(question, answer)|
-          model_editing.edit_model_templates! model_id, question, answer
-      end
-
-      db[:facts].
-        join(:fields, :factId => :id).
-        join(:fieldModels, :id => :fieldModelId).
-        select_map([:facts__id, :fieldModels__name, :fields__value]).
-        group_by(&:first).
-        each do |fact_id, grouped|
-          data = grouped.inject(Hamster.hash) { |h, (_, field, value)| h.put(field, value) }
-          card_editing.add_or_edit_card! LexicalUUID.new, model_id, data
-      end
+    rescue => e
+      puts e, e.message, e.backtrace
     end
 
     attr_accessor :model_editing, :card_editing
--- spec/05_integration/srsrb/anki_import_parser_spec.rb
+++ spec/05_integration/srsrb/anki_import_parser_spec.rb
@@ -19,29 +19,46 @@ module SRSRB
     end
 
     it "should first parse and create a model" do
-      model_editing.should_receive(:name_model!).with(an_instance_of(LexicalUUID), 'Basic')
+      model_editing.should_receive(:name_model!).with(an_instance_of(LexicalUUID), 'Hangul')
+      model_editing.should_receive(:name_model!).with(an_instance_of(LexicalUUID), 'Vocab')
+      parser.accept_upload hangul
+    end
+
+    it "should only name the models in the deck" do
+      model_editing.should_receive(:name_model!).exactly(2).times
       parser.accept_upload hangul
     end
 
     it "should create the fields for the model" do
-      model_editing.should_receive(:add_model_field!).with(an_instance_of(LexicalUUID), 'Hangul')
+      models = Hash.new { |h, model_id| h[model_id] = [] }
+      model_editing.stub(:add_model_field!) do |model_id, field|
+        models[model_id] << field
+      end
+      parser.accept_upload hangul
       # This is a typo in the original data file. Oops.
-      model_editing.should_receive(:add_model_field!).with(an_instance_of(LexicalUUID), 'Romanzied')
-      model_editing.should_receive(:add_model_field!).with(an_instance_of(LexicalUUID), 'Sound')
-      parser.accept_upload hangul
+      expect(Set.new(models.values)).to be == Set.new(
+        [%w{Term Translation Romanized}, %w{Hangul Romanzied Sound}])
     end
 
-    it "should create the fields for the same model" do
-      ids = Set.new
-      model_editing.should_receive(:name_model!) { |id, _| ids << id }
-      model_editing.should_receive(:add_model_field!) { |id, _| ids << id }
+    it "should assign the correct name to the correct model" do
+      names = Hash.new
+      models = Hash.new { |h, model_id| h[model_id] = [] }
+
+      model_editing.stub(:name_model!) do |model_id, name|
+        names[model_id] = name
+      end
+      model_editing.stub(:add_model_field!) do |model_id, field|
+        models[names.fetch(model_id)] << field
+      end
+
       parser.accept_upload hangul
 
-      expect(ids).to have(1).item
+      expect(models.fetch("Vocab")).to include("Term")
+      expect(models.fetch("Hangul")).to include("Hangul")
     end
 
-    it "should extract templates for the given model" do
-      model_editing.should_receive(:edit_model_templates!).
+    it "should extract the first templates for the given model" do
+      model_editing.should_receive(:edit_model_templates!).exactly(2).times.
         with(an_instance_of(LexicalUUID), an_instance_of(String), an_instance_of(String))
       parser.accept_upload hangul
     end
@@ -49,18 +66,24 @@ module SRSRB
     it "should create cards for each fact in the deck" do
       card_editing.should_receive(:add_or_edit_card!).
         with(an_instance_of(LexicalUUID), an_instance_of(LexicalUUID), anything).
-        exactly(37).times
+        exactly(41).times
       parser.accept_upload hangul
     end
 
     it "should create them with the given model_id" do
-      model_id = nil
+      models = {}
       card_models = {}
-      model_editing.stub(:name_model!) { |id, _| model_id = id }
+      model_editing.stub(:name_model!) { |id, name| models[id] = name }
       card_editing.stub(:add_or_edit_card!) { |id, model_id, _| card_models[id] = model_id }
 
       parser.accept_upload hangul
-      expect(Set.new(card_models.values)).to be == Set.new([model_id])
+
+      counts_by_model = card_models.map { |card, model| [models.fetch(model), card] }.
+        group_by(&:first).
+        map { |k, v| [k, v.size] }.
+        into { |kvs| Hash[kvs] }
+
+      expect(counts_by_model).to be == {'Hangul' => 37, 'Vocab' => 4}
     end
 
     it "should import the data from the deck" do
